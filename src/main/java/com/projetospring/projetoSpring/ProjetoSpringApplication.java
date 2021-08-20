6dd6c7beb5fc0a6d2b114c1f521587d869d05a12
Heitor Lima <ho_lima@outlook.com>
2021-08-20 11:00:08 -0300

    Rest testado

--- src/main/java/com/projetospring/projetoSpring/ProjetoSpringApplication.java
+++ src/main/java/com/projetospring/projetoSpring/ProjetoSpringApplication.java
@@ -1,13 +1,13 @@
-package com.projetospring.projetoSpring;
+package com.projetospring.projetospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoSpringApplication {
+public class ProjetospringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoSpringApplication.class, args);
+		SpringApplication.run(ProjetospringApplication.class, args);
 	}
 
 }
